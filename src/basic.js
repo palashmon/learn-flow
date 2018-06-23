d0b2c3d4cc3e52664e0ac2d5a097041ca66c3108
Palash Mondal <palashm@live.in>
2018-06-23 22:15:37 +0530

    Added more primitive types examples

--- src/basic.js
+++ src/basic.js
@@ -28,3 +28,67 @@ notSure = false; // okay, definitely a boolean
 // Array
 let prettySure: Array<number> = [1, 2, 3];
 // prettySure.toFixed(); // Error: Property 'toFixed' doesn't exist on type 'Array'.
+
+//-------------------------------------------------------
+// null and void
+//-------------------------------------------------------
+function acceptsNull(value: null) {
+  /* ... */
+}
+
+function acceptsUndefined(value: void) {
+  /* ... */
+}
+
+acceptsNull(null); // Works!
+//acceptsNull(undefined); // Error!
+//acceptsUndefined(null); // Error!
+acceptsUndefined(undefined); // Works!
+
+//-------------------------------------------------------
+// Maybe types
+//-------------------------------------------------------
+function acceptsMaybeString(value: ?string) {
+  // ...
+}
+
+acceptsMaybeString('bar'); // Works!
+acceptsMaybeString(undefined); // Works!
+acceptsMaybeString(null); // Works!
+acceptsMaybeString(); // Works!
+
+//-------------------------------------------------------
+// Optional object properties
+//-------------------------------------------------------
+function acceptsObject(value: { foo?: string }) {
+  // ...
+}
+
+acceptsObject({ foo: 'bar' }); // Works!
+acceptsObject({ foo: undefined }); // Works!
+//acceptsObject({ foo: null }); // Error!
+acceptsObject({}); // Works!
+
+//-------------------------------------------------------
+// Optional function parameters
+//-------------------------------------------------------
+function acceptsOptionalString(value?: string) {
+  // ...
+}
+
+acceptsOptionalString('bar'); // Works!
+acceptsOptionalString(undefined); // Works!
+//acceptsOptionalString(null); // Error!
+acceptsOptionalString(); // Works!
+
+//-------------------------------------------------------
+// Function parameters with defaults
+//-------------------------------------------------------
+function acceptsOptionalString(value: string = 'foo') {
+  // ...
+}
+
+acceptsOptionalString('bar'); // Works!
+acceptsOptionalString(undefined); // Works!
+//acceptsOptionalString(null);      // Error!
+acceptsOptionalString(); // Works!
